d64bc8d5bc68f19449a7a8138f1abfcca78e1738
Bruno Hamel <bruno.hamel93@gmail.com>
2018-02-04 14:48:07 -0500

    Update panic.go

--- panic.go
+++ panic.go
@@ -1,25 +1,17 @@
 package main
 
-
 import "fmt"
 
-func f1(arg int) (int, error) {
+func f1(arg int) int {
 	if arg == 42 {
 		panic("bad arg")
 	}
-
-	return arg + 3, nil
+	return arg + 3
 }
 
 func main() {
-
 	for _, i := range []int{7, 42} {
-		result, err := f1(i)
-		if err != nil {
-			fmt.Println("f1 failed:", err )
-		} else {
-			fmt.Println("f1 worked:", result)
-		}
+		fmt.Println("f1 worked:", f1(i))
 	}
 
 }
