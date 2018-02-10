3ce3a210b1ed802123ce665e2170c5c422553ba7
Bruno Hamel <bruno.hamel93@gmail.com>
2018-02-09 20:43:16 -0500

    Update channel.go

--- channel.go
+++ channel.go
@@ -1,22 +1,23 @@
 package main
 
-import "fmt"
-
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum // send sum to c
-}
+import (
+	"fmt"
+	 "time"
+	 "math/rand"
+	)
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
+    c := make(chan string)
+    go boring("boring!", c)
+    for i := 0; i < 5; i++ {
+        fmt.Printf("You say: %q\n", <-c) // Receive expression is just a value.
+    }
+    fmt.Println("You're boring; I'm leaving.")
+}
 
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // receive from c
-
-	fmt.Println(x, y, x+y)
+func boring(msg string, c chan string) {
+    for i := 0; ; i++ {
+        c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
+        time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+    }
 }
