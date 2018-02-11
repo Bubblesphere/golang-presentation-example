a9e6a16c76c9bbba8776c3f61dd8b98b7d1b72a5
Déric Dallaire <dallaire.deric@gmail.com>
2018-02-11 12:21:03 -0500

    Added frontend for app example

---
+++ app.js
@@ -0,0 +1,69 @@
+const endpoints = {
+  create: 'http://localhost:8000/task/create',
+  list: 'http://localhost:8000/task/list',
+  delete: 'http://localhost:8000/task/delete'
+}
+
+const createAPI = (content) => {
+  //addTodoToDom('0e879e24-9894-4dfa-73a7-031cd1dc2a21', content);
+  fetch(endpoints.create,  {
+    method: 'post',
+    headers: { 'Content-Type': 'text/plain' },
+    body: content
+  })
+  .then(response => response.json())
+  .then(json => addTodoToDom({
+    uuid: json,
+    content: content
+  }));
+}
+
+const listAPI = () => {
+  //addListToDom(JSON.parse('{"0e879e24-9894-4dfa-73a7-031cd1dc2a21":"1","32c98be7-25fe-42d7-7c93-f97e09db7a1d":"2","4fd7b310-4573-47d0-6b21-49ef614d1970":"3"}'));
+  fetch(endpoints.list,  {
+      method: 'post',
+      headers: { 'Content-Type': 'text/plain' }
+    })
+    .then(response => response.json())
+    .then(json => addListToDom(JSON.parse(json)));
+}
+
+const deleteAPI = (id) => {
+  fetch(endpoints.delete,  {
+    method: 'delete'
+  });
+}
+
+const addListToDom = (json) => {
+  Object.keys(json).forEach(function(key) {
+    addTodoToDom(key, json[key]);
+  });
+}
+
+const addTodoToDom = (uuid, content) => {
+  let div = document.createElement("div");
+  let li = document.createElement("li");
+  li.textContent = content;
+  li.setAttribute("id", uuid);
+  let del = document.createElement("input");
+  del.value = 'Delete';
+  del.type = 'button'
+  del.addEventListener('click', (e) => {
+    div.remove();
+    deleteAPI(uuid);
+  });
+  div.appendChild(li);
+  div.appendChild(del);
+  document.querySelectorAll('#todos')[0].appendChild(div);
+}
+
+const add = document.querySelectorAll('#add')[0];
+add.addEventListener("click", (e) => {
+  const content = document.querySelectorAll('#content')[0];
+  if (content.value !== '') {
+    createAPI(content.value);
+    content.value = '';
+  }
+},false);
+
+listAPI();
